d6b7d3153bd2c12742cfc95f99bdbec588169efb
Lukas Sestic <lukas.sestic@photomath.net>
2021-03-24 00:38:57 +0100

    Created a salt and pepper image version

---
+++ MasterThesis/Images/ImageHelper.swift
@@ -0,0 +1,28 @@
+//
+//  ImageHelper.swift
+//  MasterThesis
+//
+//  Created by Lukas Sestic on 24.03.2021..
+//
+
+import Foundation
+import SwiftImage
+
+struct ImageHelper {
+
+    static func addSaltPepperNoise(to image: Image<RGB<UInt8>>, withPercentage percentage: Double) -> Image<RGB<UInt8>> {
+
+        image.map { pixel in
+
+            guard Double.random(in: 0...1) <= percentage else {
+                return pixel
+            }
+
+            return Bool.random() ? RGB.white : RGB.black
+        }
+    }
+
+    static func save<T: _CGPixel>(image: Image<T>, withFormat format: ImageFormat = .png, to url: URL) throws {
+        try image.data(using: format)?.write(to: url)
+    }
+}
--- MasterThesis/main.swift
+++ MasterThesis/main.swift
@@ -9,7 +9,17 @@ import Foundation
 import SwiftImage
 
 let lenaUrl = URL(string: "/Users/lukassestic/Developer/MasterThesis/Assets/lena.png")!
+let lenaSPUrl = URL(fileURLWithPath: "/Users/lukassestic/Developer/MasterThesis/Assets/lena_salt_pepper.png")
 
-let image: Image<RGB<UInt8>>? = ImageLoader.load(from: lenaUrl)
+let image: Image<RGB<UInt8>> = ImageLoader.load(from: lenaUrl)!
+
+let image2 = ImageHelper.addSaltPepperNoise(to: image, withPercentage: 0.03)
+
+do {
+    try ImageHelper.save(image: ImageHelper.addSaltPepperNoise(to: image, withPercentage: 0.01),
+                          to: lenaSPUrl)
+} catch {
+    print("Error: \(error)")
+}
 
 print()
